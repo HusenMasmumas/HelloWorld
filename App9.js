d65a11cbc59baa5058be1abb2c8fd56a95d25782
HusenMasmumas <sen.masmumas@gmail.com>
2021-01-31 21:07:28 +0700

    new

--- App9.js
+++ App9.js
@@ -4,28 +4,34 @@ import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar,Image,backgro
 const DATA = [
   {
     id: '1',
-    title: require('./src/images/home.png') ,
-    name: 'a'
+    photo: require('./src/images/101.jpg') ,
+    name: 'บุพเพสันนิวาส',
+    view: '15200000'
+
   },
   {
     id: '2',
-    title: require('./src/images/home.png') ,
-    name: 'a'
+    photo: require('./src/images/102.jpg') ,
+    name: 'รากนครา',
+    view: '9920580'
   },
   {
     id: '3',
-    title: require('./src/images/home.png') ,
-    name: 'a'
+    photo: require('./src/images/103.jpeg') ,
+    name: 'โซ่เวรี',
+    view: '78902560'
   },
   {
     id: '3',
-    title: require('./src/images/home.png') ,
-    name: 'a'
+    photo: require('./src/images/104.jpg') ,
+    name: 'นาคี',
+    view: '9825000'
   },
   {
     id: '3',
-    title: require('./src/images/home-2.png') ,
-    name: 'a'
+    photo: require('./src/images/105.jpg') ,
+    name: 'ใบไม่ที่ปลิดปลิว',
+    view: '75200690'
   }
 ];
 
@@ -44,15 +50,15 @@ const App11 = () => {
 
            
             <View style={styles.cardStyle} >
-            <View style={{ flexDirection: 'row', margin: 16 ,}}>
-              <Image source={require('./src/images/home.png')} style={{ width: 45, height: 45, borderRadius: (45 / 2) }} />
+            <Image source={item.photo} style={{ width: '100%', height: 200 }} />
+            <View style={{ flexDirection: 'row', margin: 16 }}>
+             
               <View style={{ flexDirection: 'column', marginLeft: 16 }}>
-                <Text style={{ color: "#000000EE" }} >Title</Text>
-                <Text style={{ color: "#000000EE" }} >SubTitle</Text>
+                <Text style={{ color: "#FFFFFFEE",fontSize: 18 ,margin: 2,marginLeft:10}} > {item.name}</Text>
+                <Text style={{ color: "#CDCDCDEE" }} >ผู้ชม {item.view} View</Text>
                 
               </View>
             </View>
-            <Image source={item.title} style={{ width: '100%', height: 200 }} />
           </View>
           
         )}
@@ -76,13 +82,15 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 32,
-  },cardStyle:
+  },
+  cardStyle:
   {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: 'rgba(105, 210, 231, 0.75)',
+    backgroundColor: 'rgba(100, 100, 100, 1)',
     marginBottom: 20,
-    borderRadius: 20
+    borderRadius: 20,
+    margin: 20
   }
 });
 
